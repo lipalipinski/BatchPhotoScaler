37541b05ea711fde35ce6c553259e12241aff460
JL <lipabass@gmail.com>
2022-07-10 22:02:18 +0200

    it works

---
+++ photo_scaler.py
@@ -0,0 +1,91 @@
+"""
+this script scales all .jpg and .jpeg files at given path
+to the given (biger) dimension, and saves them in new folder
+with a name corresponding to that dimension
+"""
+
+import os
+import re
+from PIL import Image
+
+
+def get_path():
+    """
+    asks for a folder path
+    checkec if it's valid
+    returns a folder path (string)
+    """
+    print("Welcome to photo scaller!")
+    while True:
+        path = input("Image folder path: ")
+        if os.path.isdir(path):
+            return path
+        else:
+            print("Folder path incorrect!")
+
+
+def get_dimension():
+    """
+    asks for a maximum dimension
+    returns int
+    """
+    while True:
+        try:
+            dim = int(input("Max dimension (px): "))
+            break
+        except ValueError:
+            print("Incorrect value!")
+    return dim
+
+
+def mk_photo_dir(path, dim):
+    """
+    creates a folder /[dimension]px in a foto folder
+    """
+    os.mkdir(path + "/" + str(dim) + "px")
+    return
+
+
+def locate_photos(path):
+    """
+    returns a list of .jpg and jpeg 
+    files at given path, case insensitive
+    """
+    photo_list =[]
+    list_dir = os.listdir(path)
+    for guess in list_dir:
+
+        pat = re.compile("(?i)(jpe?g)$") 
+        if re.search(pat, guess):
+            photo_list.append(guess)
+
+    return photo_list
+
+
+def scale_photo(photo: Image, max_dim):
+    """
+    returnes scaled down image
+    """
+    h, w = photo.size
+    factor = max_dim / max(w, h)
+    new_h = int(h * factor)
+    new_w = int(w * factor)
+    photo = photo.resize((new_h, new_w))
+    return photo
+
+
+def main():
+    path = get_path()
+    max_dim = get_dimension()
+    mk_photo_dir(path, max_dim)
+    photos = locate_photos(path)
+    for name in photos:
+        img = Image.open(path + "/" + name)
+        img = scale_photo(img, max_dim)
+        img.save(path + "/" + str(max_dim) + "px/" + name)
+
+
+
+
+if __name__ == "__main__":
+    main()
